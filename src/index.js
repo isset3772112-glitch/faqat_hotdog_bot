75f174fc75b889f04f0a09143bb5f721eab3987d
isset3772112-glitch <isset3772112@gmail.com>
2026-09-09 03:23:04 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,25 +14,11 @@ export default {
         const message = update.message;
         const chatId = message.chat.id;
 
-        if (message.text === "/id") {
-  await sendMessage(
-    env.BOT_TOKEN,
-    chatId,
-    "🆔 Chat ID: " + chatId
-  );
-
   return new Response("OK");
         }
         
         const text = message.text || "";
         const caption = message.caption || "";
-
-        if (text === "/id") {
-  await sendMessage(
-    env.BOT_TOKEN,
-    chatId,
-    "🆔 Chat ID: " + chatId
-  );
 
   return new Response("OK");
         }
